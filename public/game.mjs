d503ff4f6ad20b1b3fb37d42e7d65b0a5760bc5d
Phomolo Benedict Tshaba <benedicttshaba@gmail.com>
2024-06-06 12:35:34 +0000

    Intial Commit

--- public/game.mjs
+++ public/game.mjs
@@ -1,6 +1,124 @@
-import Player from './Player.mjs';
-import Collectible from './Collectible.mjs';
-
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+import Player from './Player.mjs';
+import Collectible from './Collectible.mjs';
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 500;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+
+const getPlayerRandomColor = () => {
+  let r = Math.floor(Math.random() * 256),
+      g = Math.floor(Math.random() * 256),
+      b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+let initialColor = getPlayerRandomColor();
+context.fillStyle = initialColor;
+
+let allPlayers = [], player, food;
+
+socket.on('connect', function () {
+
+  let playerId = socket.io.engine.id;
+  player = new Player({
+    x: Math.floor(Math.random() * CANVAS_WIDTH - 30),
+    y: Math.floor(Math.random() * CANVAS_HEIGHT - 30),
+    score: 0,
+    id: playerId,
+  });
+
+  socket.emit("start", player);
+
+  socket.on("player_updates", (players) => {
+    allPlayers = players;
+    drawPlayers(allPlayers);
+  });
+
+  socket.on("food", (newFood) => {
+    food = newFood;
+    drawFood(food.x, food.y, food.value);
+  });
+
+  window.addEventListener("keydown", (e) => {
+    let cur_key = e.key.toLowerCase();
+    let direction = cur_key === "d" ? "right" :
+      cur_key === "a" ? "left" :
+        cur_key === "w" ? "up" :
+          cur_key === "s" ? "down" : null;
+
+    if (direction) {
+      context.clearRect(...getCoord(player));
+      player.movePlayer(direction, 10);
+      checkBoundary(player);
+      context.fillRect(...getCoord(player));
+      allPlayers = allPlayers.map(p => {
+        if (p.id === player.id) {
+          return player;
+        } else {
+          return p;
+        }
+      });
+      socket.emit("player_updates", allPlayers);
+    }
+
+    if (player.collision(food)) {
+      context.clearRect(...getFoodCoord(food));
+      food = { value: 0 };
+      context.fillRect(...getCoord(player));
+      socket.emit("collision", player);
+      let rank = player.calculateRank(allPlayers);
+      document.getElementById("rank").innerText = rank;
+    }
+  })
+
+});
+
+const getCoord = (player) => {
+  return [player.x, player.y, 20, 20];
+};
+
+const checkBoundary = (player) => {
+  if (player.x < 5) {
+    player.x = 5;
+  }
+  if (player.x > CANVAS_WIDTH - 25) {
+    player.x = CANVAS_WIDTH - 25;
+  }
+  if (player.y < 5) {
+    player.y = 5;
+  }
+  if (player.y > CANVAS_HEIGHT - 25) {
+    player.y = CANVAS_HEIGHT - 25;
+  }
+};
+
+const drawFood = (x, y, value) => {
+  const foodColours = ["#f542cb", "#f55742", "#f5f242", "#428df5", "#42f56c"];
+  context.beginPath();
+  context.arc(x, y, value * 2 + 10, 0, 2 * Math.PI, false);
+  context.fillStyle = foodColours[value - 1];
+  context.fill();
+};
+
+const getFoodCoord = (food) => {
+  const radFactor = food.value * 2 + 10;
+  return [
+    food.x - radFactor,
+    food.y - radFactor,
+    food.x + radFactor,
+    food.y + radFactor
+  ];
+};
+
+const drawPlayers = (players) => {
+  for (let p of players) {
+    context.fillRect(...getCoord(p));
+  }
+};
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
+const nocache = require('nocache');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
@@ -15,6 +17,17 @@ app.use('/assets', express.static(process.cwd() + '/assets'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  helmet({
+    noSniff: true,
+    xssFilter: true,
+    hidePoweredBy: {
+      setTo: 'PHP 7.4.3',
+    },
+  })
+);
+app.use(nocache());
 
 //For FCC testing purposes and enables user to connect from outside the hosting platform
 app.use(cors({origin: '*'})); 
@@ -52,5 +65,53 @@ const server = app.listen(portNum, () => {
     }, 1500);
   }
 });
+
+const io = socket.listen(server);
+const Collectible = require("./public/Collectible.mjs");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 500;
+
+let players = [],
+     foodID = 0,
+     food;
+
+io.on("connection", (socket) => {
+  socket.on("start", (player) => {
+    console.log("player has joined", player);
+
+    
+    players.push(player);
+
+    socket.emit("player_updates", players);
+
+    food = createFood(foodID);
+    socket.emit("food", food);
+  });
+
+  socket.on("collision", (player) => {
+    for (let p of players) {
+      if (p.id === player.id) {
+        p.score += food.value;
+      }
+    }
+    food = createFood(foodID);
+    socket.emit("food", food);
+  });
+});
+
+const createFood = (id) => {
+  let random_x = Math.floor(Math.random() * (CANVAS_WIDTH  - 20)) + 20,
+      random_y = Math.floor(Math.random() * (CANVAS_HEIGHT - 20)) + 20,
+      random_v = Math.floor(Math.random() * 5) + 1;
+
+  foodID += 1;
+
+  return new Collectible({
+    x: random_x,
+    y: random_y,
+    value: random_v,
+    id: id
+  });
+};
 
 module.exports = app; // For testing
